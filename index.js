d3223869494d58f09ae2eab251ecc0e96a6e0d2c
Deepak Kumar <deekumar.d22@gmail.com>
2025-10-17 18:11:07 +0000

    added index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express(); //app is a object which return express() function. but if you check the typeof app it will show function.
+
+const dotenv = require("dotenv");
+dotenv.config();
 const PORT = process.env.PORT || 3000;
+
+app.use("/home", (req, res) => {
+  return res.status(200).json({
+    message: "route testing",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port-${PORT}`);
