2b0233e454789d0c110445638b5badfc0f636bdc
Deepak Kumar <deekumar.d22@gmail.com>
2025-11-06 18:22:01 +0000

    movie route

--- route/movie.routes.js
+++ route/movie.routes.js
@@ -1,32 +1,7 @@
-const Movie = require("../models/movie.model.js");
+const createMovie = require("../controllers/movie.controllers.js");
 
 const movie = (app) => {
-  app.post("/api/v1/mba", async (req, res) => {
-    const body = req.body;
-    console.log("some one calling me");
-
-    try {
-      // Create and save the new movie
-      const newMovie = new Movie(body);
-      await newMovie.save();
-
-      return res.status(201).json({
-        success: "true",
-        error: {},
-        response: newMovie,
-        message: "Successfully created new movie",
-      });
-    } catch (err) {
-      console.error(err);
-
-      return res.status(500).json({
-        success: "true",
-        error: err.message,
-        response: {},
-        message: "Something went wrong",
-      });
-    }
-  });
+  app.post("/mba/api/v1/movie", createMovie);
 };
 
 module.exports = movie;
