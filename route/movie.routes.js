be36e62b87e85b4aa2910ec46a15e32db46a8a5a
Deepak Kumar <deekumar.d22@gmail.com>
2025-11-13 14:24:59 +0000

    get movie by id

--- route/movie.routes.js
+++ route/movie.routes.js
@@ -1,7 +1,18 @@
-const createMovie = require("../controllers/movie.controllers.js");
+const {
+  createMovie,
+  listMovieController,
+  getMovieById,
+} = require("../controllers/movie.controllers.js");
 
 const movie = (app) => {
+  // Create movie
   app.post("/mba/api/v1/movie", createMovie);
+
+  // List all movies
+  app.get("/mba/api/v1/movies", listMovieController);
+
+  // Get movie by ID (either as path param or query param)
+  app.get("/mba/api/v1/movie/:id", getMovieById);
 };
 
-module.exports = movie;
+module.exports = { movie };
