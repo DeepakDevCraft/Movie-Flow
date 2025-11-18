d65789d367d4530fdae00fded2cad3e24d5c308f
Deepak Kumar <deekumar.d22@gmail.com>
2025-11-18 14:10:18 +0000

    added delete movie controller

--- controllers/movie.controllers.js
+++ controllers/movie.controllers.js
@@ -80,6 +80,42 @@ const listMovieController = async (req, res) => {
   }
 };
 
-module.exports = { createMovie, listMovieController, getMovieById };
+const deleteMovieById = async (req, res) => {
+  try {
+    const { id } = req.params; // extract id from route params
+
+    const movie = await Movie.delete(id);
+
+    if (!movie) {
+      return res.status(404).json({
+        success: false,
+        error: "Movie not found",
+        response: {},
+        message: "No movie found with the given ID",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      error: {},
+      response: movie,
+      message: "Movie deleted successfully",
+    });
+  } catch (err) {
+    console.error("❌ Error deleting movie by ID:", err);
+    return res.status(500).json({
+      success: false,
+      error: err.message,
+      response: {},
+      message: "Something went wrong while deleting the movie",
+    });
+  }
+};
+module.exports = {
+  createMovie,
+  listMovieController,
+  getMovieById,
+  deleteMovieById,
+};
 
 //module.exports = { createMovie, listMovieController };
