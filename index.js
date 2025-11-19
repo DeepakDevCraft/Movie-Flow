a3bc0a72da9893606e0dc8babfe587d506e37325
Deepak Kumar <deekumar.d22@gmail.com>
2025-11-19 18:24:44 +0000

    remove test route

--- index.js
+++ index.js
@@ -12,10 +12,6 @@ const PORT = process.env.PORT || 3000;
 
 movie(app); // ✅ includes both POST and GET
 
-app.use("/home", (req, res) => {
-  res.status(200).json({ message: "route testing" });
-});
-
 app.listen(PORT, async () => {
   console.log(`Server is running on port-${PORT}`);
   connection(); // ✅ ensure DB connects before handling requests
